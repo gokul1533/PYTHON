4ce390197f941435b73a78078ad5a6bc943f2f3c
gokul1533 <126935796+gokul1533@users.noreply.github.com>
2023-03-15 13:48:00 +0530

    Add files via upload

---
+++ array 15.3.2023.py
@@ -0,0 +1,9 @@
+n=int(input("enter the elements in an array,\n"))
+arr=[]
+print("enter the elements")
+for i in range (0,n):
+    s=int(input())
+    arr.append(s)
+print(arr)    
+    
+               
